d8992c2c5ae99838c1dad6b552406417f9a62baf
Dominic Zappala <BB_Boy@Monicas-MacBook-Pro.local>
2018-08-04 18:41:04 -0400

    implemented data returns for weather results (finally -_-)

--- src/js/app.js
+++ src/js/app.js
@@ -1,16 +1,33 @@
-import { apiKey } from "./config.js"
+import { apiKey } from "./config.js";
 
-$(document).ready(function() {
-    
-$('#weatherSearch').click((event) => { 
-    event.preventDefault();   
-    let value = document.getElementById('weatherText').value
-    fetch("http://api.openweathermap.org/data/2.5/weather?q=" + value + "&units=imperial&APPID=" + apiKey).then(function(response) {
-        //const result = response;
-        $("#weather").text(response.weather[0].description);
-    });//.catch(function() {alert("weather data could not be retrieved, please check your query and try again")});
+$(document).ready(function () {
+
+    $('#weatherSearch').click((event) => {
+        event.preventDefault();
+        let value = document.getElementById('weatherText').value
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + value + "&type=like&units=imperial&APPID=" + apiKey).then(function (response) {
+            console.log("response", response);
+            return response.json();
+        }).then(function (response) {
+            console.log("response", response);
+            $("#location").text(response.name);
+            $("#weather").text(response.weather[0].main);
+            $("#temperature").text("Temperature " + response.main.temp + '&deg;');
+            $("#maxTemp").text("High " + response.main.temp_max + '&deg;');
+            $("#minTemp").text("Low " + response.main.temp_min + '&deg;');
+            $("#windSpeed").text("Wind Speed: " + response.wind.speed + " mph");
+            $("#humidity").text("Humidity " + response.main.humidity + "%");
+            if((response.rain || response.snow) === undefined) {
+                $("#percipitation").css("display: none");
+            } else if (response.rain) {
+                $("#percipitation").text("Rain " + response.rain + " in");
+            } else {
+                $("#percipitation").text("Snow " + response.snow + " in");
+            };
+
+        }); //.catch(function() {alert("weather data could not be retrieved, please check your query and try again")});
+    });
+
+
+
 });
-
-
-
-})
