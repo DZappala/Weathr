65239e58059aa0c7a8bea84be4ff54881634f096
Dominic Zappala <BB_Boy@Monicas-MacBook-Pro.local>
2018-08-09 16:21:29 -0400

    added fuzzy-search with the google places API, added WebPack functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,23 @@
+// imports api keys from super secret config file [hehehe]
 import {
     apiKey,
     ipstackKey,
-    timezonedbKey
+    timezonedbKey,
+    googleMapsKey
 } from "./config.js";
 
 $(document).ready(function () {
 
+    // Handles content on pageload
+    // Fetches the lat and lon data based on user IP address
     fetch("http://api.ipstack.com/check?access_key=" + ipstackKey).then(function (response) {
         console.log("response")
         return response.json();
     }).then(function (response) {
         let currentLat = response.latitude;
         let currentLon = response.longitude;
+
+        // Uses current lat and lon to determine current weather
         fetch("https://api.openweathermap.org/data/2.5/weather?lat=" + currentLat + "&lon=" + currentLon + "&type=like&units=imperial&APPID=" + apiKey).then(function (response) {
             console.log("response", response);
             return response.json();
@@ -19,6 +25,11 @@ $(document).ready(function () {
             console.log("response", response);
             var now = new Date();
             var hour = now.getHours();
+
+            // Removes the image if there already is one ## WARNING REMOVES ALL IMG ELEMENTS FROM PAGE ### ( we need to fix this *sigh* )
+            $('img').remove();
+
+            // Inserts various weather data into Divs basedon ID 
             $("#location").text(response.name);
             $("#weather").text(response.weather[0].main);
             $("#temperature").text("Temperature " + response.main.temp + ' ℉');
@@ -34,6 +45,7 @@ $(document).ready(function () {
                 $("#percipitation").text("Snow " + response.snow + " in");
             };
 
+            // Logic decides what picture to show next to the Temperature Description (isthisAI.jpg)
             if (response.weather[0].id < 299) {
                 $("#weatherImg").prepend('<img src="/img/004-thunderstorm.svg" />')
             } else if (response.weather[0].id >= 300 && response.weather[0].id < 399) {
@@ -62,70 +74,97 @@ $(document).ready(function () {
         });
     })
 
+
+    // Handles content after each serch query
     $('#weatherSearch').click((event) => {
+
         event.preventDefault();
         let value = document.getElementById('weatherText').value // TODO autocorrect wrong search queries
         // TODO handle country requests? (maybe two different search boxes (city, country) )
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + value + "&type=like&units=imperial&APPID=" + apiKey).then(function (response) {
-            console.log("response", response);
-            return response.json();
+
+        // Autocomplete search Query from googleMaps API
+        fetch('https://maps.googleapis.com/maps/api/place/autocomplete/json?input=' + value + '&types=(cities)&key=' + googleMapsKey, {
+            mode: 'no-cors'
         }).then(function (response) {
             console.log("response", response);
-            let resultLat = response.coord.lat;
-            let resultLon = response.coord.lon;
-            let hour = fetch("http://api.timezonedb.com/v2/get-time-zone?key=" + timezonedbKey + "&format=json&by=position&lat=" + resultLat + "&lng=" + resultLon).then(function (response) {
-                console.log("resposne", response);
+            return response;
+        }).then(function (response) {
+            console.log("response", response);
+            let query = response.predictions[0].terms[0].value + ',' + response.predictions[0].terms[(terms.length)].value;
+
+            // fetch data from OpenWeatherMap API 
+            fetch("https://api.openweathermap.org/data/2.5/weather?q=" + query + "&type=like&units=imperial&APPID=" + apiKey).then(function (response) {
+                console.log("response", response);
                 return response.json();
             }).then(function (response) {
-                let timeSeconds = new Date((response.timestamp * 1000) + response.gmtOffset);
-                let hour = timeSeconds.getUTCHours();
-                return hour;
-            });
-            console.log(hour);
-            $('img').remove();
-            $("#location").text(response.name);
-            $("#weather").text(response.weather[0].main);
-            $("#temperature").text("Temperature " + response.main.temp + ' ℉');
-            $("#maxTemp").text("High " + response.main.temp_max + ' ℉');
-            $("#minTemp").text("Low " + response.main.temp_min + ' ℉');
-            $("#windSpeed").text("Wind Speed: " + response.wind.speed + " mph");
-            $("#humidity").text("Humidity " + response.main.humidity + "%");
-            if ((response.rain || response.snow) === undefined) {
-                $("#percipitation").css("display: none");
-            } else if (response.rain) {
-                $("#percipitation").text("Rain " + response.rain + " in");
-            } else {
-                $("#percipitation").text("Snow " + response.snow + " in");
-            };
+                console.log("response", response);
+                let resultLat = response.coord.lat;
+                let resultLon = response.coord.lon;
 
-            if (response.weather[0].id < 299) {
-                $("#weatherImg").prepend('<img src="/img/004-thunderstorm.svg" />')
-            } else if (response.weather[0].id >= 300 && response.weather[0].id < 399) {
-                $("#weatherImg").prepend('<img src="/img/011-rainy-1.svg" />')
-            } else if (response.weather[0].id >= 500 && response.weather[0].id < 504) {
-                $("#weatherImg").prepend('<img src="/img/023-forecast.svg" />')
-            } else if (response.weather[0].id >= 511 && response.weather[0].id < 532) {
-                $("#weatherImg").prepend('<img src="/img/013-rain.svg" />')
-            } else if (response.weather[0].id >= 600 && response.weather[0].id < 699 && !611 && !612) {
-                $("#weatherImg").prepend('<img src="/img/009-snow.svg" />')
-            } else if (response.weather[0].id === 611 || response.weather[0].id === 612) {
-                $("#weatherImg").prepend('<img src="/img/010-sleet.svg" />')
-            } else if (response.weather[0].id >= 700 && response.weather[0].id < 799) {
-                $("#weatherImg").prepend('<img src="/img/024-foggy.svg" />')
-            } else if (response.weather[0].id === 800 && (hour < 7 || hour > 19)) {
-                $("#weatherImg").prepend('<img src="/img/015-moon.svg" />')
-            } else if (response.weather[0].id === 800 && (hour >= 7 && hour <= 19)) {
-                $("#weatherImg").prepend('<img src="/img/032-sun.svg" />') // TODO sun.svg not displaying 
-            } else if (response.weather[0].id === 801 && (hour < 7 || hour > 19)) {
-                $("#weatherImg").prepend('<img src="/img/031-cloud.svg" />')
-            } else if (response.weather[0].id === 801 && (hour >= 7 && hour <= 19)) {
-                $("#weatherImg").prepend('<img src="/img/014-overcast.svg" />')
-            } else if (response.weather[0].id >= 802) {
-                $("#weatherImg").prepend('<img src="/img/014-overcast.svg" />')
-            };
+                // fetch timezone data of previous query from TimeZoneDB API
+                let hour = fetch("http://api.timezonedb.com/v2/get-time-zone?key=" + timezonedbKey + "&format=json&by=position&lat=" + resultLat + "&lng=" + resultLon).then(function (response) {
+                    console.log("resposne", response);
+                    return response.json();
+                }).then(function (response) {
+                    let timeSeconds = new Date((response.timestamp * 1000) + response.gmtOffset);
+                    let hour = timeSeconds.getUTCHours();
+                    return hour;
+                });
+
+                // Logs the current hour of the queried timezone for later determining if it's after sunset or not
+                console.log(hour);
+
+                // Removes the image if there already is one ## WARNING REMOVES ALL IMG ELEMENTS FROM PAGE ### ( we need to fix this *sigh* )
+                $('img').remove();
+
+                // Inserts various weather data into Divs basedon ID 
+                $("#didYouMean").text(value)
+                $("#location").text(response.name);
+                $("#weather").text(response.weather[0].main);
+                $("#temperature").text("Temperature " + response.main.temp + ' ℉');
+                $("#maxTemp").text("High " + response.main.temp_max + ' ℉');
+                $("#minTemp").text("Low " + response.main.temp_min + ' ℉');
+                $("#windSpeed").text("Wind Speed: " + response.wind.speed + " mph");
+                $("#humidity").text("Humidity " + response.main.humidity + "%");
+
+                // Logic determining if it's raining or not
+                if ((response.rain || response.snow) === undefined) {
+                    $("#percipitation").css("display: none"); // TODO is supposed to remove the element from the page but .remove() did not work so like idk, maybe lets try again
+                } else if (response.rain) {
+                    $("#percipitation").text("Rain " + response.rain + " in");
+                } else {
+                    $("#percipitation").text("Snow " + response.snow + " in");
+                };
+
+                // Logic decides what picture to show next to the Temperature Description (isthisAI.jpg)
+                if (response.weather[0].id < 299) {
+                    $("#weatherImg").prepend('<img src="/img/004-thunderstorm.svg" />')
+                } else if (response.weather[0].id >= 300 && response.weather[0].id < 399) {
+                    $("#weatherImg").prepend('<img src="/img/011-rainy-1.svg" />')
+                } else if (response.weather[0].id >= 500 && response.weather[0].id < 504) {
+                    $("#weatherImg").prepend('<img src="/img/023-forecast.svg" />')
+                } else if (response.weather[0].id >= 511 && response.weather[0].id < 532) {
+                    $("#weatherImg").prepend('<img src="/img/013-rain.svg" />')
+                } else if (response.weather[0].id >= 600 && response.weather[0].id < 699 && !611 && !612) {
+                    $("#weatherImg").prepend('<img src="/img/009-snow.svg" />')
+                } else if (response.weather[0].id === 611 || response.weather[0].id === 612) {
+                    $("#weatherImg").prepend('<img src="/img/010-sleet.svg" />')
+                } else if (response.weather[0].id >= 700 && response.weather[0].id < 799) {
+                    $("#weatherImg").prepend('<img src="/img/024-foggy.svg" />')
+                } else if (response.weather[0].id === 800 && (hour < 7 || hour > 19)) {
+                    $("#weatherImg").prepend('<img src="/img/015-moon.svg" />')
+                } else if (response.weather[0].id === 800 && (hour >= 7 && hour <= 19)) {
+                    $("#weatherImg").prepend('<img src="/img/032-sun.svg" />') // TODO sun.svg not displaying 
+                } else if (response.weather[0].id === 801 && (hour < 7 || hour > 19)) {
+                    $("#weatherImg").prepend('<img src="/img/031-cloud.svg" />')
+                } else if (response.weather[0].id === 801 && (hour >= 7 && hour <= 19)) {
+                    $("#weatherImg").prepend('<img src="/img/014-overcast.svg" />')
+                } else if (response.weather[0].id >= 802) {
+                    $("#weatherImg").prepend('<img src="/img/014-overcast.svg" />')
+                };
+            });
         });
     });
-
 
 
 });
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/js/app.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
